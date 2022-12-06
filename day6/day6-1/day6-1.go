c6cfc610bcb35e7d165879da7e373986b190738a
wupeiing <wupeiing@gmail.com>
2022-12-07 01:07:55 +0800

    update day6

---
+++ day6/day6-1/day6-1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	readFile, err := os.Open("../day6.txt")
+  
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
+ 
+    fileScanner.Split(bufio.ScanLines)
+  
+	secs := ""
+    for fileScanner.Scan() {
+		secs = fileScanner.Text()
+    }
+	var sol [27]int
+	for i, _ := range secs {
+		ans := false
+		sol = [27]int{}
+		for j := 0; j<4; j++ {
+			t := secs[i+j] - 'a'
+			if sol[t] == 0 {
+				sol[t]++
+			} else {
+				ans = false
+				break
+			}
+			ans = true
+		}
+		if ans == true {
+			fmt.Println(i+4)
+			break
+		}
+	}
+    readFile.Close()
+}
